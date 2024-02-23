e4fa307ddc347d05fd5bf39bfc7dfbf8e24c8c13
troy-chen <492794628@qq.com>
2024-02-23 19:43:46 +0800

    完成条件，任务基础模型

--- org.wxd.boot.core/src/main/java/org/wxd/boot/core/lang/task/Condition.java
+++ org.wxd.boot.core/src/main/java/org/wxd/boot/core/lang/task/Condition.java
@@ -1,6 +1,6 @@
 package org.wxd.boot.core.lang.task;
 
-import com.alibaba.fastjson.annotation.JSONField;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -17,54 +17,18 @@ import java.io.Serializable;
 @Getter
 @Setter
 @Accessors(chain = true)
+@AllArgsConstructor
 public class Condition extends ObjectBase implements Serializable {
 
     /** 条件1 */
-    private int k1;
+    private final int k1;
     /** 条件2 */
-    private int k2;
+    private final int k2;
     /** 条件3 */
-    private int k3;
+    private final int k3;
     /** 当前完成条件变更方案 */
-    private ChangeType changeType;
-    /** 目标 如果等于-1 表示不限制 */
-    private long target;
-    /** 当前进度 */
-    private long progress;
-
-    public boolean change(int k1, int k2, int k3, long progress) {
-
-        if (this.k1 != k1) return false;
-        if (this.k2 != 0 && this.k2 != k2) return false;
-        if (this.k3 != 0 && this.k3 != k3) return false;
-
-        if (this.target > 0 && this.progress >= this.target) return false;
-
-        switch (changeType) {
-            case Add: {
-                this.progress = Math.addExact(this.progress, progress);
-            }
-            break;
-            case Replace: {
-                this.progress = progress;
-            }
-            break;
-            case Min: {
-                this.progress = Math.min(this.progress, progress);
-            }
-            break;
-            case Max: {
-                this.progress = Math.max(this.progress, progress);
-            }
-            break;
-        }
-
-        return true;
-    }
-
-    @JSONField(serialize = false, deserialize = false)
-    public boolean finish() {
-        return this.target >= this.progress;
-    }
+    private final ChangeType changeType;
+    /** 目标进度 如果等于-1 表示不限制 */
+    private final long target;
 
 }
