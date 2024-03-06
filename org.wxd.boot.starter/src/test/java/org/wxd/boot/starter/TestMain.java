9c17f5115d70ab3aec2cc30b921f79ce97426551
wuxindao <492794628@qq.com>
2024-03-06 20:43:01 +0800

    屏蔽代码

--- org.wxd.boot.starter/src/test/java/org/wxd/boot/starter/TestMain.java
+++ org.wxd.boot.starter/src/test/java/org/wxd/boot/starter/TestMain.java
@@ -12,10 +12,10 @@ import org.wxd.boot.core.str.xml.XmlUtil;
 public class TestMain {
 
     public static void main(String[] args) {
-        BootConfig bootConfig = new BootConfig();
+        /*BootConfig bootConfig = new BootConfig();
         bootConfig.getHttp().getHeaders().add(new WebConfig.Header().setKey("1").setValue("2"));
-        System.out.println(XmlUtil.toXml(bootConfig));
-        Starter.startBoot(TestMain.class);
+        System.out.println(XmlUtil.toXml(bootConfig));*/
+        Starter.startBoot(TestMain.class/*其实这里需要包含的包名*/);
         Starter.start(true, 1, "test");
     }
 
